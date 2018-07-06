f75fbfe24cb188643ff8bf65dcc0a36465c1a554
unknown <sen.vikram5@gmail.com>
2018-07-06 20:18:27 +0530

    first commit

---
+++ src/main/java/com/boot/ng/h2/bootngh2/BootNgH2Application.java
@@ -0,0 +1,12 @@
+package com.boot.ng.h2.bootngh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootNgH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootNgH2Application.class, args);
+	}
+}
---
+++ src/test/java/com/boot/ng/h2/bootngh2/BootNgH2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.boot.ng.h2.bootngh2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootNgH2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
